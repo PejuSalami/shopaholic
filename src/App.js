a6d17e2178b97657c3b3035e92a886f5258f42f0
Peju <olapejusal@gmail.com>
2023-06-11 22:45:23 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <Header />
-      <Home /> */}
       <Router>
         <Switch>
           <Route exact path="/checkout" component={Checkout} />
